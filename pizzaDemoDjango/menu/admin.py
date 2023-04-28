6079fa1fab70a7d6d1818c2cf7c82ee3ec2d1dd6
Florian Gasparine <gasparine.florian@gmail.com>
2023-04-28 20:50:58 +0200

    add menu page

--- pizzaDemoDjango/menu/admin.py
+++ pizzaDemoDjango/menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pizza
 
-# Register your models here.
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ingredients', 'vegetarienne', 'prix')
+    search_fields = ['ingredients']
+
+
+admin.site.register(Pizza, PizzaAdmin)
